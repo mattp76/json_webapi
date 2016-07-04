741c8f9683e30c3b8e2286738a4d1d492a9a83ed
maparker <maparker@bauer-media.com.au>
2016-07-04 11:12:15 +1000

    updates to web JSON api test

--- json_webapi/Controllers/JsonController.cs
+++ json_webapi/Controllers/JsonController.cs
@@ -16,24 +16,29 @@ namespace json_webapi.Controllers
     public class JsonController : ApiController
     {
         // POST api/json
+        //The webApiConfig (app_start) was modified to send the root URL directly to this method
+        //I was using fiddler to help me complete this task
+        //The JSON will serialise into the 'req' requestModel.Request object on post
         [HttpPost]
         public HttpResponseMessage ProcessJson(RequestModel.Request req)
         {
-            HttpResponseMessage response = new HttpResponseMessage();
-            ResponseModel res = new ResponseModel();
-
+            //Add code into the try / catch to grab any parsing errors or exceptions
             try
             {
                 if (req != null)
                 {
+                    ResponseModel res = new ResponseModel();
                     res.response = new List<ResponseSet>();
-  
+
+                    //check the request has some payload items
                     if (req.Payload.Length > 0)
                     {
                         foreach (var p in req.Payload)
                         {
+                            //check drm is true and episode count is greater than 0
                             if (p.Drm && p.EpisodeCount > 0)
                             {
+                                //build our response object
                                 res.response.Add(new ResponseSet
                                 {
                                     image = p.Image.ShowImage,
@@ -42,25 +47,33 @@ namespace json_webapi.Controllers
                                 });
                             }
                         }
-
-                        string json = JsonConvert.SerializeObject(res);
-                        response = Request.CreateResponse(HttpStatusCode.OK);
-                        response.Content = new StringContent(json, Encoding.Unicode);                   
+                        //return a valid response
+                        return ValidResponse(res);
                     }
-                }
-                else
-                {
-                    return ErrorResponse();
-
                 }
             }
             catch (Exception ex)
             {
+                //Might like also to log / capture exceptions here 'ex'
+                //Return error, likely a parsing issue - Bad request
                 return ErrorResponse();
             }
 
+            //No request object - return bad request (although this could be changed, as it wasnt stricly a bad request but an empty request body)
+            return ErrorResponse();
+        }
+
+
+        public HttpResponseMessage ValidResponse(ResponseModel res)
+        {
+            string json = JsonConvert.SerializeObject(res);
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(json, Encoding.Unicode);
             return response;
         }
+
 
         public HttpResponseMessage ErrorResponse()
         {
